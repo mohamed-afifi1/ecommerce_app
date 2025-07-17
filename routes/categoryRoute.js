8a93ca143f6a920aee3392dfb9916679285ad0f7
mohamed-afifi1 <mohammed2004koki@gmail.com>
2025-07-17 05:08:48 +0300

    category

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { newcategory } = require('../services/categoryService')
+const { newcategory, getcategories, getcategory, updatecategory, deletecategory } = require('../services/categoryService')
 
 const router = express.Router();
 
-router.post('/', newcategory )
+router.post('/', newcategory ).get('/', getcategories).get('/:id', getcategory)
+.post('/:id', updatecategory).delete('/:id', deletecategory);
 
 module.exports = router;
